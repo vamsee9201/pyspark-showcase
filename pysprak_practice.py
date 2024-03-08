c8c67330eca05bee236c41706c48cff3756dcc02
vamsee9201 <vamseekrishna9201@gmail.com>
2024-03-07 23:54:15 -0600

    did some practice

--- pysprak_practice.py
+++ pysprak_practice.py
@@ -1,8 +1,9 @@
+#%%
 from pyspark.sql import SparkSession
 from datetime import datetime, date
 import pandas as pd
 from pyspark.sql import Row
-
+#%%
 spark = SparkSession.builder.getOrCreate()
 df = spark.createDataFrame([
     Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
@@ -10,3 +11,45 @@ df = spark.createDataFrame([
     Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
 ])
 df
+# %%
+df = spark.createDataFrame([
+    (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+    (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+    (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0))
+], schema='a long, b double, c string, d date, e timestamp')
+df
+# %%
+pandas_df = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [2., 3., 4.],
+    'c': ['string1', 'string2', 'string3'],
+    'd': [date(2000, 1, 1), date(2000, 2, 1), date(2000, 3, 1)],
+    'e': [datetime(2000, 1, 1, 12, 0), datetime(2000, 1, 2, 12, 0), datetime(2000, 1, 3, 12, 0)]
+})
+df = spark.createDataFrame(pandas_df)
+df
+# %%
+df.show()
+# %%
+df.printSchema()
+# %%
+df.show(1)
+# %%
+spark.conf.set('spark.sql.repl.eagerEval.enabled', True)
+df
+# %%
+df.show(1, vertical=True)
+
+# %%
+df.columns
+
+# %%
+df.printSchema()
+
+# %%
+df.select("a", "b","c").describe().show()
+
+# %%
+df.collect()
+
+# %%
