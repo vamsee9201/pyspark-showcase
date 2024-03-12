462c8e19f205dee900844f389d09d4fcd0273069
vamsee9201 <vamseekrishna9201@gmail.com>
2024-03-12 17:07:21 -0500

    added groupby function

--- pysprak_practice.py
+++ pysprak_practice.py
@@ -82,3 +82,37 @@ def pandas_plus_one(series: pd.Series) -> pd.Series:
 
 df.select(pandas_plus_one(df.a)).show()
 # %%
+def pandas_filter_func(iterator):
+    for pandas_df in iterator:
+        yield pandas_df[pandas_df.a == 1]
+
+df.mapInPandas(pandas_filter_func, schema=df.schema).show()
+
+# %%
+df = spark.createDataFrame([
+    ['red', 'banana', 1, 10], ['blue', 'banana', 2, 20], ['red', 'carrot', 3, 30],
+    ['blue', 'grape', 4, 40], ['red', 'carrot', 5, 50], ['black', 'carrot', 6, 60],
+    ['red', 'banana', 7, 70], ['red', 'grape', 8, 80]], schema=['color', 'fruit', 'v1', 'v2'])
+df.show()
+#%%
+df.groupby('color').avg().show()
+# %%
+def plus_mean(pandas_df):
+    return pandas_df.assign(v1=pandas_df.v1 - pandas_df.v1.mean())
+
+df.groupby('color').applyInPandas(plus_mean, schema=df.schema).show()
+# %%
+df1 = spark.createDataFrame(
+    [(20000101, 1, 1.0), (20000101, 2, 2.0), (20000102, 1, 3.0), (20000102, 2, 4.0)],
+    ('time', 'id', 'v1'))
+
+df2 = spark.createDataFrame(
+    [(20000101, 1, 'x'), (20000101, 2, 'y')],
+    ('time', 'id', 'v2'))
+
+def merge_ordered(l, r):
+    return pd.merge_ordered(l, r)
+
+df1.groupby('id').cogroup(df2.groupby('id')).applyInPandas(
+    merge_ordered, schema='time int, id int, v1 double, v2 string').show()
+#%%
