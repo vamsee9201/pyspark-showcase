0734f4571844f44b2c13a98dcad68734febc7b90
vamsee9201 <vamseekrishna9201@gmail.com>
2024-03-13 21:06:08 -0500

    pandas API practice

--- spark_pandas_API.py
+++ spark_pandas_API.py
@@ -8,3 +8,40 @@ s = ps.Series([1, 3, 5, np.nan, 6, 8])
 s
 
 # %%
+psdf = ps.DataFrame(
+    {'a': [1, 2, 3, 4, 5, 6],
+     'b': [100, 200, 300, 400, 500, 600],
+     'c': ["one", "two", "three", "four", "five", "six"]},
+    index=[10, 20, 30, 40, 50, 60])
+
+psdf
+# %%
+dates = pd.date_range('20130101', periods=6)
+dates
+# %%
+pdf = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+pdf
+# %%
+psdf = ps.from_pandas(pdf)
+type(psdf)
+psdf
+# %%
+psdf.dtypes
+
+# %%
+psdf.head()
+# %%
+psdf.index
+# %%
+psdf.columns
+
+# %%
+psdf.to_numpy()
+# %%
+psdf.describe()
+# %%
+psdf.T
+# %%
+psdf.sort_index(ascending=False)
+# %%
+psdf.sort_values(by='B')
